8406064f5d2c2244a52bf67770b6251a7102bc03
somsisas <148601455+somsisas@users.noreply.github.com>
2024-02-14 01:25:12 -0700

    Fixed errors

--- phase1.c
+++ phase1.c
@@ -1,5 +1,5 @@
 /*
- Name- Soumay Agarwal
+ Name- Soumay Agarwal & Cezar Rata
  Course- CSC 452
  Description- This is the phase1a of the assignment, which implements the phase1_init, spork, join,
               quit_phase1a, getpid, dumpProcesses and TEMP_switchTo. This part is responsible for creating processes
@@ -22,6 +22,7 @@ int occupiedSlots = 0;
 Process *curProcess;
 
 int globalPid = 2;
+processQueue priortyQueues[7];
 
 /**
  * This function is responsible for setting up or initializing our process table.
@@ -43,7 +44,38 @@ void setUpInit()
         processTable[i].runQueueNext = NULL;
         processTable[i].nextSibling = NULL;
         processTable[i].firstChild = NULL;
+        processTable[i].waitingOnChild = false;
+        processTable[i].startTime = 0;
     }
+    for (int i = 0; i < 7; i++)
+    { // Initialize run list
+        priortyQueues[i].head = NULL;
+        priortyQueues[i].tail = NULL;
+    }
+}
+
+void enableInterrupts()
+{
+    USLOSS_PsrSet(3);
+}
+
+void disableInterrupts()
+{
+    if ((USLOSS_PsrGet() & 1) == 0)
+    {
+        USLOSS_PsrSet(0);
+    }
+    else
+    {
+        USLOSS_PsrSet(1);
+    }
+}
+
+int getCurTime(void)
+{
+
+    int val = currentTime();
+    return val;
 }
 
 /**
@@ -70,6 +102,7 @@ void modeChecker(char *caller)
 
 void phase1_init(void)
 {
+    USLOSS_Console("IN init\n");
     modeChecker("phase1_init");
     setUpInit();
     // setting up innit process
@@ -81,6 +114,8 @@ void phase1_init(void)
     processTable[1].stack = malloc(USLOSS_MIN_STACK);
     processTable[1].tableStatus = OCCUPIED;
     // curProcess = &processTable[1];
+    priortyQueues[6].head = &processTable[1];
+    priortyQueues[6].tail = &processTable[1];
     russ_ContextInit(processTable[1].pid, &(processTable[1].context), processTable[1].stack, USLOSS_MIN_STACK, &init_main, NULL);
     occupiedSlots += 1;
 }
@@ -97,9 +132,29 @@ void phase1_init(void)
  * return: pid of the created child
  */
 
+void addQueue(Process *proc)
+{
+
+    if (priortyQueues[proc->priority].head == NULL)
+    {
+        priortyQueues[proc->priority].head = priortyQueues[proc->priority].tail = proc;
+        proc->runQueueNext = NULL;
+    }
+    else
+    {
+        Process *tmp = priortyQueues[proc->priority].tail;
+        if (tmp->runQueueNext == NULL)
+        {
+            tmp->runQueueNext = proc;
+        }
+        priortyQueues[proc->priority].tail = proc;
+    }
+}
+
 int spork(char *name, int (*startFunc)(char *), char *arg,
           int stacksize, int priority)
 {
+    USLOSS_Console("IN SPORK\n");
     modeChecker("spork");
     if (stacksize < USLOSS_MIN_STACK)
     {
@@ -154,6 +209,14 @@ int spork(char *name, int (*startFunc)(char *), char *arg,
         processTable[slot].nextSibling = tmpProcess;
     }
 
+    addQueue(&(processTable[slot]));
+
+    if (strcmp("sentinel", name) != 0 && strcmp("testcase_main", name) != 0)
+    {
+        disableInterrupts();
+        dispatcher();
+        enableInterrupts();
+    }
     return processTable[slot].pid;
 }
 
@@ -164,79 +227,119 @@ int spork(char *name, int (*startFunc)(char *), char *arg,
  * status- the out pointer which is given the dead child status
  * return: pid of the dead child
  */
-
 int join(int *status)
 {
+    USLOSS_Console("IN JOIN\n");
     modeChecker("join");
+
     if (status == NULL)
+    {
         return -3;
+    }
 
     if (curProcess->firstChild == NULL)
+    {
+        return -2; // No children to wait for
+    }
+
+    Process *child = curProcess->firstChild;
+    Process *prev = NULL;
+    Process *terminatedChild = NULL;
+
+    while (child != NULL)
+    {
+        if (child->endStatus == TERMINATED)
+        {
+            terminatedChild = child;
+            break;
+        }
+        prev = child;
+        child = child->nextSibling;
+    }
+
+    // fIf no terminated child is found, block the current process
+    if (terminatedChild == NULL)
+    {
+        blockMe(WAITING_ON_CHILD);
+
+        // after being unblocked, search for the terminated child again
+        child = curProcess->firstChild;
+        prev = NULL;
+        while (child != NULL)
+        {
+            if (child->endStatus == TERMINATED)
+            {
+                terminatedChild = child;
+                break;
+            }
+            prev = child;
+            child = child->nextSibling;
+        }
+    }
+
+    if (terminatedChild == NULL)
     {
         return -2;
     }
 
-    // Checking for dead children of the curProcess
-    Process *genPointer = curProcess->firstChild;
-    // prev pointer will hold the dead child
-    Process *prev;
-    // if the firstchild is dead, pid points to it and the curProcess makes the next sibling
-    // the first child
-    if (genPointer->endStatus == TERMINATED)
+    *status = terminatedChild->runnableStatus;
+
+    if (prev == NULL)
     {
-        curProcess->firstChild = curProcess->firstChild->nextSibling;
-        prev = genPointer;
+        curProcess->firstChild = terminatedChild->nextSibling;
     }
-    // otherwise we find the dead child by taversing the next siblings
     else
     {
-        while (genPointer->nextSibling->endStatus != TERMINATED)
-        {
-            genPointer = genPointer->nextSibling;
-        }
-        prev = genPointer->nextSibling;
-        genPointer->nextSibling = genPointer->nextSibling->nextSibling;
+        prev->nextSibling = terminatedChild->nextSibling;
     }
-    int curpid = prev->pid;
-    *status = processTable[curpid % MAXPROC].runnableStatus;
-    // freeing the child process stack and other fields, so that
-    // we can reuse the slot
-    free(processTable[curpid % MAXPROC].stack);
-    occupiedSlots -= 1;
-    processTable[curpid % MAXPROC].name = "";
-    processTable[curpid % MAXPROC].pid = 0;
-    processTable[curpid % MAXPROC].priority = 0;
-    processTable[curpid % MAXPROC].parentPid = -50;
-    processTable[curpid % MAXPROC].tableStatus = EMPTY;
-    processTable[curpid % MAXPROC].runnableStatus = RUNNABLE;
-    processTable[curpid % MAXPROC].stack = NULL;
-    processTable[curpid % MAXPROC].runQueueNext = NULL;
-    processTable[curpid % MAXPROC].nextSibling = NULL;
-    processTable[curpid % MAXPROC].firstChild = NULL;
-    return curpid;
+
+    int terminatedPid = terminatedChild->pid;
+
+    free(terminatedChild->stack);
+    terminatedChild->name = "";
+    terminatedChild->pid = 0;
+    terminatedChild->priority = 0;
+    terminatedChild->parentPid = -1;
+    terminatedChild->tableStatus = EMPTY;
+    terminatedChild->runnableStatus = INITIALIZED;
+    terminatedChild->stack = NULL;
+    terminatedChild->runQueueNext = NULL;
+    terminatedChild->nextSibling = NULL;
+    terminatedChild->firstChild = NULL;
+
+    occupiedSlots--;
+
+    return terminatedPid;
 }
 
 /**
  * This function is responsible for termination the current process by setting its
  * end status as terminated. The runnable status of the current process is changed to
- * the status passed. Due to the lack of dispatcher we use Temp switch to switch to
- * another process.
+ * the status passed. It then calls dispatcher to switch processes
  * status- provided status
- * switchToPid- provided pid of process to switch to
  */
-
-void quit_phase_1a(int status, int switchToPid)
+void quit(int status)
 {
-    modeChecker("quit_phase_1a");
+    USLOSS_Console("IN QUIT\n");
+    modeChecker("quit");
+
     if (curProcess->firstChild != NULL)
     {
         USLOSS_Console("ERROR: Process pid %d called quit() while it still had children.\n", curProcess->pid);
         USLOSS_Halt(0);
     }
+
     curProcess->endStatus = TERMINATED;
     curProcess->runnableStatus = status;
 
-    TEMP_switchTo(switchToPid);
+    Process *parent = &processTable[curProcess->parentPid % MAXPROC];
+    if (parent->runnableStatus == WAITING_ON_CHILD)
+    {
+        // unblock the parent if it's waiting for a child to terminate
+        unblockProc(curProcess->parentPid);
+    }
+
+    dispatcher();
 }
 
 /**
@@ -283,24 +386,164 @@ void dumpProcesses(void)
     }
 }
 
-/**
- * This function is responsible for context switching to a process whose pid is provided.
- * pid- the provided pid of a process
+/*
+ * This is a helper function that adds process to the priortyQueues
+ * proc: the process being added to the run list.
  */
 
-void TEMP_switchTo(int pid)
+void removeQueue(Process *proc)
 {
-    modeChecker("TEMP_switchTo");
-    if (pid == 1)
+    if (priortyQueues[proc->priority].head == proc)
     {
-        curProcess = &(processTable[1]);
-        USLOSS_ContextSwitch(NULL, &(processTable[1].context));
+        priortyQueues[proc->priority].head = priortyQueues[proc->priority].head->runQueueNext;
     }
     else
     {
-        int slotPosition = pid % MAXPROC;
-        Process *temp = curProcess;
-        curProcess = &(processTable[slotPosition]);
-        USLOSS_ContextSwitch(&(temp->context), &(processTable[slotPosition].context));
+        Process *headNode = priortyQueues[proc->priority].head;
+        while (headNode->runQueueNext != proc)
+        {
+            headNode = headNode->runQueueNext;
+        }
+        headNode->runQueueNext = headNode->runQueueNext->runQueueNext;
     }
+}
+
+void moveBackQueue(Process *proc)
+{
+    if (priortyQueues[proc->priority].head != priortyQueues[proc->priority].tail)
+    {
+        Process *tmp1 = priortyQueues[proc->priority].head;
+        priortyQueues[proc->priority].head = tmp1->runQueueNext;
+        Process *tmp2 = priortyQueues[proc->priority].tail;
+        priortyQueues[proc->priority].tail = tmp1;
+        tmp2->runQueueNext = tmp1;
+    }
+}
+
+void dispatcher(void)
+{
+    // USLOSS_Console("IN DISP\n");
+    disableInterrupts();
+    if (curProcess == NULL)
+    {
+        curProcess = &(processTable[1]);
+        processTable[1].runnableStatus = RUNNING;
+        curProcess->startTime = getCurTime();
+        USLOSS_ContextSwitch(NULL, &(processTable[1].context));
+    }
+    for (int i = 1; i < 7; i++)
+    {
+        if (priortyQueues[i].head != NULL)
+        {
+            // USLOSS_Console("Entering here upper cur %d  %d\n", curProcess->priority, priortyQueues[i].head->priority);
+            if (curProcess->priority != priortyQueues[i].head->priority)
+            {
+                // USLOSS_Console("Entering here\n");
+                USLOSS_Context oldContext = curProcess->context;
+                if (curProcess->runnableStatus == RUNNING && curProcess->endStatus != TERMINATED)
+                {
+                    curProcess->runnableStatus = RUNNABLE;
+                }
+                curProcess = priortyQueues[i].head;
+                curProcess->runnableStatus = RUNNING;
+                curProcess->startTime = getCurTime();
+                USLOSS_ContextSwitch(&(oldContext), &(curProcess->context));
+                break;
+            }
+            else
+            {
+                // if (curProcess->runnableStatus == RUNNING)
+                // {
+                // USLOSS_Console("Entering here\n");
+                if (priortyQueues[i].head->runQueueNext != NULL)
+                {
+                    USLOSS_Console("Entering here\n");
+                    int appropriateTime = currentTime() - priortyQueues[i].head->startTime;
+                    if (appropriateTime >= 80000)
+                    {
+                        USLOSS_Console("Here %d\n", appropriateTime);
+                        Process *tempHead = priortyQueues[i].head;
+                        tempHead->runnableStatus = RUNNABLE;
+                        moveBackQueue(tempHead);
+                        curProcess = priortyQueues[i].head;
+                        curProcess->runnableStatus = RUNNABLE;
+                        USLOSS_ContextSwitch(&(tempHead->context), &(curProcess->context));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    USLOSS_Console("Entering here\n");
+                    break;
+                }
+                // }
+                // else
+                // {
+                //     break;
+                // }
+            }
+        }
+    }
+
+    // Special case when time runs out for program.
+    // if (curProcess->runnableStatus == RUNNING && (getCurTime() - curProcess->startTime) >= 80000)
+    // {
+    //     current->run_state = RUNNABLE;
+    //     moveBackQueue();
+    //     current->next_proc = NULL;
+    //     addQueue(curProcess);
+    // }
+    enableInterrupts();
+}
+
+/**
+ * Blocks the current process with the specified newStatus.
+ * The process will remain blocked until it is unblocked by unblockProc.
+ * The newStatus must be greater than 10.
+ *
+ * newStatus- The reason for blocking the process.
+ */
+void blockMe(int newStatus)
+{
+    USLOSS_Console("IN BLOCK ME\n");
+    modeChecker("blockMe");
+
+    if (newStatus <= 10)
+    {
+        USLOSS_Console("ERROR: blockMe called with invalid newStatus <= 10.\n");
+        USLOSS_Halt(0);
+    }
+
+    curProcess->runnableStatus = newStatus;
+    dispatcher();
+}
+
+/**
+ * Unblocks the process specified by pid.
+ * The process must have been previously blocked by blockMe.
+ *
+ * pid- The PID of the process to unblock.
+ * returns 0 on success, -2 if the process is not blocked or does not exist.
+ */
+int unblockProc(int pid)
+{
+    USLOSS_Console("IN UNBLOCK\n");
+    modeChecker("unblockProc");
+
+    int slot = pid % MAXPROC;
+    Process *proc = &processTable[slot];
+
+    if (proc->pid != pid || proc->runnableStatus <= 10)
+    {
+        return -2;
+    }
+
+    proc->runnableStatus = RUNNABLE;
+
+    addQueue(proc);
+    dispatcher();
+    return 0;
 }
